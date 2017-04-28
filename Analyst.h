d9c5ee3000f9b48be8a3910c4eac314f9ac83dfa
Aris  Emery <arisemery@MacBook-Pro-4.local>
2017-04-27 19:39:13 -0600

    intializing repo

---
+++ Analyst.h
@@ -0,0 +1,14 @@
+//
+// Created by Aris  Emery on 4/27/17.
+//
+
+#ifndef ANALYSTCOMPARER_ANALYST_H
+#define ANALYSTCOMPARER_ANALYST_H
+
+
+class Analyst {
+
+};
+
+
+#endif //ANALYSTCOMPARER_ANALYST_H
